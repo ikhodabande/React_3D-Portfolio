d6eb62e5fb8f16df7f61097f90df6d301e556f50
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-21 09:27:23 +0330

    up

--- 3d_portfolio/src/App.jsx
+++ 3d_portfolio/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
-App = () => {
- 
 
+const App = () => {
   return (
    <div>
     3D Developer portfolio 
